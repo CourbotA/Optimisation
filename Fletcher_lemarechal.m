8dba4748d063da62ae1637dc309439e8b456570d
Tomas <tomas.bonilla-clavijo@telecom-st-etienne.fr>
2021-10-25 19:26:40 +0200

    fletcher marechal sans recherche de min

--- Fletcher_lemarechal.m
+++ Fletcher_lemarechal.m
@@ -1,10 +1,13 @@
-function [] = Fletcher_lemarechal(f,tau,x0,eps)
+function [xres] = Fletcher_lemarechal(f,fp,x0,eps)
+% Fletcher_lemarechal prend une fonction f, son gradient fp, un point de
+% depart x0 et un seuil eps pour retourner la coordonnee qui minimise la
+% fonction avec la methode des plus fortes pentes.
+
 xk = x0;
-  s= tau; 
-    if ( -1 <= xk < 0 )
-        s = - tau ; 
-    end
-    
-    Result = f(xk + s
-      
+while (abs(fp(xk))> eps)
+  dk = -fp(xk); 
+  ak = 0; %Recherche de ak tq f(xk + ak*dk) soit min 
+  xk = xk + ak*dk;
+end
+    xres = xk   
 end
