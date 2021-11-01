fed6726378f5f873f9baf0bfff9b93c09fb13beb
Tomas <tomas.bonilla-clavijo@telecom-st-etienne.fr>
2021-11-01 21:37:40 +0100

    Fletcher

--- Fletcher_lemarechal.m
+++ Fletcher_lemarechal.m
@@ -1,4 +1,4 @@
-function [xres] = Fletcher_lemarechal(f,fp,x0,eps)
+function [xres] = Fletcher_lemarechal(f,fp,x0,eps,p)
 % Fletcher_lemarechal prend une fonction f, son gradient fp, un point de
 % depart x0 et un seuil eps pour retourner la coordonnee qui minimise la
 % fonction avec la methode des plus fortes pentes.
@@ -6,8 +6,12 @@ function [xres] = Fletcher_lemarechal(f,fp,x0,eps)
 xk = x0;
 while (abs(fp(xk))> eps)
   dk = -fp(xk); 
-  ak = 0; %Recherche de ak tq f(xk + ak*dk) soit min 
+  ak = p;
+  xk2 = xk;
+  while (f(xk2)> f(xk2+p*dk))
+    xk2 = xk2 + p*dk;
+  end
   xk = xk + ak*dk;
 end
-    xres = xk   
+    xres = xk; 
 end
